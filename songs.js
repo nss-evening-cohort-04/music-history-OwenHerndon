8727947a7b8596482d912e08f0d1c79b6bc5622e
Owen Herndon <owenherndon@Owens-MacBook-Pro.local>
2016-08-18 17:21:09 -0500

    added js file

---
+++ songs.js
@@ -0,0 +1,24 @@
+var songs = [];
+
+songs[songs.length] = "Legs - by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song - by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall - by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welcome to the Jungle - by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c - by Alanis Morisette on the album Jagged Little Pill";
+
+var songDisplay = "";
+for (var i = 0; i < songs.length; i++) {
+
+	}
+
+
+// var songDisplay = "";
+// for (var i = 0; i < songs.length; i++) {
+//   songDisplay += "<div class='productCard'>";
+//   songDisplay += "<h3>" + musicProducts[i].title + "</h3>";
+//   productCard += "<img class ='picture' src= ' " + musicProducts[i].png + " '>";
+//   productCard += "<p class='price'>" + musicProducts[i].price + "</p>";
+//   productCard += "<p class='description'>" + musicProducts[i].description + "</p>";
+//   productCard +="</div>";
+
+};
